a922e52448fb561e243595b52937d6e442e34b70
mannsodha <72299351+mannsodha@users.noreply.github.com>
2020-10-23 19:49:58 +0530

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,31 @@
+class Pig {
+    constructor (x,y){
+        var options ={
+            isStatic: false,
+            restitution: 0.8,
+            density:0.8,
+            friction:1
+        }
+    
+        this.body = Bodies.rectangle(x,y,50,50,options);
+        this.width = 50;
+        this.height = 50;
+        World.add(world,this.body);  
+       
+     
+    }
+    
+    display (){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate (angle);
+        fill("green");
+        strokeWeight(4);
+        stroke("green");
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+         pop();
+    }
+    }
